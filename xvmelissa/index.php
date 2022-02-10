6eef5323736874f55c21ca905645058f910f50a2
Cesar Casas Quiroga <cesar@deltadevs.com>
2022-02-10 12:33:51 -0600

    +SLN VS

---
+++ xvmelissa/index.php
@@ -0,0 +1,24 @@
+<?php
+
+// Kickstart the framework
+$f3=require('lib/base.php');
+
+$f3->set('DEBUG',1);
+if ((float)PCRE_VERSION<8.0)
+	trigger_error('PCRE version is out of date');
+
+// Load configuration
+$f3->config('config.ini');
+
+if($f3->get('DEBUG') == 0) {
+	$f3->set('ruta', 'http://' . $f3->get('HOST') . '/apps/regina/');
+}
+else {
+	$f3->set('ruta', 'http://' . $f3->get('HOST') . ':' . $f3->get('PORT') . '/');
+}
+
+$f3->route('GET /', function ($f3) {
+	echo \Template::instance()->render('Invitacion.html');
+});
+
+$f3->run();
